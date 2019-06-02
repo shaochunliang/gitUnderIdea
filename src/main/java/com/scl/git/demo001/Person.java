cf79cf9235864ea43383f542a10c4fa9b1349610
scl <2234283277@qq.com>
2019-06-02 10:58:13 +0800

    啊啊啊2

--- src/main/java/com/scl/git/demo001/Person.java
+++ src/main/java/com/scl/git/demo001/Person.java
@@ -22,6 +22,14 @@ public class Person {
         this.age = age;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
